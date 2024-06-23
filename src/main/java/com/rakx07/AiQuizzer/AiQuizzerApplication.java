659794143c32ac77d501210d705e6c6716946fc0
rakx07 <rakib15-13846@diu.edu.bd>
2024-06-23 23:48:08 +0600

    Project Setup Done.

---
+++ src/main/java/com/rakx07/AiQuizzer/AiQuizzerApplication.java
@@ -0,0 +1,13 @@
+package com.rakx07.AiQuizzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiQuizzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiQuizzerApplication.class, args);
+	}
+
+}
